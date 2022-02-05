af6c3c876c99c5c85c82a44645e8478c5ebe4e41
Gabor Javorszky <gabor@javorszky.co.uk>
2022-02-05 19:02:11 +0000

    decode stuff

--- netlify/functions/generate-stripe-link.js
+++ netlify/functions/generate-stripe-link.js
@@ -1,5 +1,26 @@
+const base64 = require("base64url");
+
 exports.handler = async function (event, context) {
   console.log("this is a thing here", JSON.stringify(event.body));
+
+  accessToken = JSON.stringify({ message: event.body });
+  parts = accessToken.split(".");
+
+  header = parts[0];
+  payload = parts[1];
+  signature = parts[2];
+
+  decodedHeader = Buffer.from(header, "base64").toString("ascii");
+  decodedPayload = Buffer.from(payload, "base64").toString("ascii");
+  decodedSignature = Buffer.from(signature, "base64").toString("ascii");
+
+  console.log(
+    "decodeds",
+    JSON.stringify(decodedHeader),
+    JSON.stringify(decodedPayload),
+    JSON.stringify(decodedSignature),
+  );
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: event.body }),
