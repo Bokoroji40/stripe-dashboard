c9033dd593ea0701bb330afa52af8ba111affe44
Gabor Javorszky <gabor@javorszky.co.uk>
2022-02-05 23:41:07 +0000

    Add request to supabase

--- netlify/functions/generate-stripe-link.js
+++ netlify/functions/generate-stripe-link.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto");
+const https = require("https");
 
 exports.handler = async function (event, context) {
   accessToken = event.body;
@@ -28,6 +29,34 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ message: "jwt signature verification failed" }),
     };
   }
+
+  const req = https.request(
+    "https://uavpsmlmcsfcplfxuubi.supabase.co/rest/v1/stripe_customers",
+    {
+      method: "GET",
+      headers: {
+        apikey: accessToken,
+        Authorization: "Bearer " + process.env.SUPA_ANON_KEY,
+      },
+    },
+    (res) => {
+      console.log(`STATUS: ${res.statusCode}`);
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
+        console.log(`BODY: ${chunk}`);
+      });
+      res.on("end", () => {
+        console.log("No more data in response.");
+      });
+    },
+  );
+
+  req.on("error", (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  req.end();
 
   return {
     statusCode: 200,
