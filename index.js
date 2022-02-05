3af509feb312326858e33f8b8987fea591509d10
Gabor Javorszky <gabor@javorszky.co.uk>
2022-02-05 12:11:51 +0000

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+var SUPABASE_URL = "https://uavpsmlmcsfcplfxuubi.supabase.co";
+var SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQzMTI5NTc1LCJleHAiOjE5NTg3MDU1NzV9.e0aZ2SUi8lpURmx72EmqKSZgvmAUYazp28Tus7PKl6Y";
+
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var signUpForm = document.querySelector("#sbform");
+  signUpForm.onsubmit = signUpSubmitted.bind(signUpForm);
+});
+
+const signUpSubmitted = (event) => {
+  event.preventDefault();
+  console.log(event);
+  const email = event.target[0].value;
+
+  supabase.auth
+    .signUp({ email, password })
+    .then((response) => {
+      response.error ? alert(response.error.message) : setToken(response);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
