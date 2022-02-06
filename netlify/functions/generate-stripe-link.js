027404c7db765477bba32cbe8f050c0dfc5ccdd2
Gabor Javorszky <gabor@javorszky.co.uk>
2022-02-06 00:56:34 +0000

    Pass customer id to stripe

--- netlify/functions/generate-stripe-link.js
+++ netlify/functions/generate-stripe-link.js
@@ -23,11 +23,9 @@ exports.handler = async function (event, context) {
   }
 
   const stripeID = await getStripeIDFromSupabase(accessToken);
+  const link = await getStripeSessionLink(stripeID);
 
-  console.log("this is the stripe id", stripeID);
-  //   const link = await getStripeSessionLink(stripeID);
-
-  //   console.log("this is the link that came back", link);
+  console.log("this is the link that came back", link);
 
   return {
     statusCode: 200,
