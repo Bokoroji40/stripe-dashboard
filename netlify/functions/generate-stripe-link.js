3d31c9c27b461c4cfdf9e88538eca51539156020
Gabor Javorszky <gabor@javorszky.co.uk>
2022-02-06 00:20:32 +0000

    Replace node https with axios

--- netlify/functions/generate-stripe-link.js
+++ netlify/functions/generate-stripe-link.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-const https = require("https");
+const axios = require("axios").default;
 const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
 exports.handler = async function (event, context) {
@@ -36,41 +36,19 @@ exports.handler = async function (event, context) {
 };
 
 const getStripeIDFromSupabase = async function (accessToken) {
-  const req = https.request(
-    "https://uavpsmlmcsfcplfxuubi.supabase.co/rest/v1/stripe_customers",
-    {
-      method: "GET",
+  axios
+    .get("https://uavpsmlmcsfcplfxuubi.supabase.co/rest/v1/stripe_customers", {
       headers: {
         apikey: process.env.SUPA_ANON_KEY,
         Authorization: "Bearer " + accessToken,
       },
-    },
-    (res) => {
-      let rdata = "";
-      console.log(`STATUS: ${res.statusCode}`);
-      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-      res.setEncoding("utf8");
-      res.on("data", (chunk) => {
-        rdata += chunk;
-        console.log(`BODY: ${chunk}`);
-      });
-      res.on("end", () => {
-        console.log("No more data in response.");
-
-        console.log("the entire data is this");
-        console.warn(rdata);
-
-        return rdata;
-      });
-    },
-  );
-
-  req.on("error", (e) => {
-    console.error(`problem with request: ${e.message}`);
-    return "";
-  });
-
-  req.end();
+    })
+    .then((response) => {
+      console.warn("this is the axios response", response);
+    })
+    .catch((error) => {
+      console.error("so axios errored: ", error);
+    });
 };
 
 const getStripeSessionLink = async function (stripeID) {
