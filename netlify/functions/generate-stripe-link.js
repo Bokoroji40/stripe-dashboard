3d30f27669c8680bc60be43996f86c8b0e8b4943
Gabor Javorszky <gabor@javorszky.co.uk>
2022-02-11 23:11:34 +0000

    Remove unnecessary logs

--- netlify/functions/generate-stripe-link.js
+++ netlify/functions/generate-stripe-link.js
@@ -42,7 +42,6 @@ const getStripeIDFromSupabase = async function (accessToken) {
       },
     })
     .then((response) => {
-      console.info("got a response");
       if (response.status !== 200) {
         throw new Error("response from supabase is not 200");
       }
@@ -53,31 +52,21 @@ const getStripeIDFromSupabase = async function (accessToken) {
 
       custID = [];
 
-      console.log("repsonse data is", response.data);
-
       for (const cust_record of response.data) {
-        console.log("cust record", cust_record);
-        console.log("\npushing", cust_record.stripe_customer_id);
         custID.push(cust_record.stripe_customer_id);
       }
-      console.log("this is custID after the loop", custID);
     })
     .catch((error) => {
       console.error("so axios errored: ", error);
     });
 
-  console.info("returning custID, hopefully after the loop?", custID);
   return custID;
 };
 
 const getStripeSessionLink = async function (stripeIDs) {
   let sessions = [];
 
-  console.info("incoming stripe ids", stripeIDs);
-
   for (const id of stripeIDs) {
-    console.log("--- id: ", id);
-
     const session = await stripe.billingPortal.sessions.create({
       customer: id,
       return_url: process.env.FRONTEND_URL,
